faaf2c587d21a47e280a0e61f601277a67b68f86
do-ald533 <douglas.aldred533@gmail.com>
2024-07-05 03:18:12 +0000

    feat: initial config

---
+++ src/__main__.py
@@ -0,0 +1,17 @@
+from datetime import datetime, date
+from pyspark.sql import SparkSession, Row
+
+spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
+
+# Sample data
+df = spark.createDataFrame(
+    [
+        Row(a=1, b=2.0, c="string1", d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+        Row(a=2, b=3.0, c="string2", d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+        Row(a=4, b=5.0, c="string3", d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0)),
+    ]
+)
+df.show()
+
+# Stop the Spark session
+spark.stop()
